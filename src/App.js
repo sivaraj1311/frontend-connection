f0bdff1f400842369942e094a0de1bba15d662d5
sivaraj <158682363+sivaraj1311@users.noreply.github.com>
2024-05-24 06:52:19 +0000

    haii

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 
+import { useEffect,useState } from 'react';
 import './App.css';
 
 function App() {
+  const [value,setValue]=useState("");
+  useEffect(()=>{
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow"
+    };
+    
+    fetch("https://vscodelink.vercel.app/", requestOptions)
+      .then((response) => response.text())
+      .then((result) => setValue(result))
+      .catch((error) => console.error(error));
+  },[]);
   return (
     <>
     <h1>this is my page</h1>
